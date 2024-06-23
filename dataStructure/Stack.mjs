cb5df8d64a5b15d67ef396fc9f5569ec7bdf3a70
hyunjungKoo <sunny@swingby.us>
2024-06-23 12:57:19 +0900

    add: dataStructure/Node, LinkedList 추가

---
+++ dataStructure/Stack.mjs
@@ -0,0 +1,9 @@
+import { Node } from './Node.mjs';
+
+class Stack {
+  constructor() {
+
+  }
+}
+
+export { Stack }
---
+++ dataStructure/test_LinkedList.mjs
@@ -0,0 +1,30 @@
+import { LinkedList } from './LinkedList.mjs';
+
+let list = new LinkedList();
+
+console.log('>>> insertAt() 5번 호출')
+list.insertAt(0, 0);
+list.insertAt(1, 1);
+list.insertAt(2, 2);
+list.insertAt(3, 3);
+list.insertAt(4, 4);
+list.printAll()
+
+console.log('>>> insertLast() 실행')
+list.insertLast(5);
+list.printAll();
+
+console.log('>>> deleteAt() 실행')
+list.deleteAt(2);
+list.printAll();
+
+console.log('>>> deleteLast() 실행')
+list.deleteLast();
+list.printAll();
+
+console.log('>>> getNode() 실행')
+list.getNodeAt(3)
+
+console.log('>>> clear() 실행')
+list.clear();
+list.printAll();
