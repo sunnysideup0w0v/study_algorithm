8e2c98df526455dbb8c559778071c4c4cdd08583
sunnysideup0w0v <723973972538@naver.com>
2024-08-11 16:26:32 +0900

    [자료구조] Red-Black 트리: 삽입/제거를 위한 보조 함수 구현

--- dataStructure/RedBlackTree/RedBlackTree.mjs
+++ dataStructure/RedBlackTree/RedBlackTree.mjs
@@ -1,4 +1,4 @@
-import { BinaryTree } from '../BinaryTree/BinaryTree.mjs';
+import { BinaryTree, RED, BLACK } from './BinaryTree.mjs';
 
 // 이진탐색트리는 이미 작성한 이진트리 구조를 기반으로 확장
 class BinarySearchTree {
@@ -25,17 +25,51 @@ class BinarySearchTree {
   }
 
   // 자식 노드 교체
-  replaceParentsChild(parent, oldChlid, newChild) {
+  replaceParentsChild(parent, oldChild, newChild) {
+    if (parent === null) {
+      // node가 rootNode일 경우
+      this.root = newChild;
+    } else if (parent.getLeftSubTree() === oldChild) {
+      parent.setLeftSubTree(newChild);
+    } else if (parent.getRightSubTree() === oldChild) {
+      parent.setRightSubTree(newChild);
+    }
+
+    if (newChild !== null) newChild.setParent(parent)
   }
 
   // 부모, 자식노드가 오른쪽으로 쭉 뻗어져있는 경우 사용
   rotateLeft(node) {
+    let parent = node.getParentTree();
+    let rightChild = node.getRightSubTree();
 
+    node.setRightSubTree(rightChild.getLeftSubTree());
+
+    if (rightChild.getLeftSubTree() != null) {
+      rightChild.getLeftSubTree().setParent(node);
+    }
+
+    rightChild.setLeftSubTree(node);
+    node.setParent(rightChild);
+
+    this.replaceParentsChild(parent, node, rightChild);
   }
 
   // 부모, 자식노드가 왼쪽으로 쭉 뻗어져있는 경우 사용
   rotateRight(node) {
+    let parent = node.getParentTree();
+    let leftChild = node.getLeftSubTree();
 
+    node.setRightSubTree(leftChild.getRightSubTree());
+
+    if (leftChild.getRightSubTree() != null) {
+      leftChild.getRightSubTree().setParent(node);
+    }
+
+    leftChild.setRightSubTree(node);
+    node.setParent(leftChild);
+
+    this.replaceParentsChild(parent, node, leftChild);
   }
 }
 
