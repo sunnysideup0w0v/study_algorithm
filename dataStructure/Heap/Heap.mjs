dd6c3c5c183cf2e6b2b8c80d1e46fa255694f45f
sunnysideup0w0v <723973972538@naver.com>
2024-08-21 22:32:33 +0900

    [자료구조] Heap의 구현(삽입)

--- dataStructure/Heap/Heap.mjs
+++ dataStructure/Heap/Heap.mjs
@@ -32,14 +32,83 @@ class Heap {
     // 삽입된 노드와 부모 노드의 우선순위를 비교하여 알맞은 위치로 이동
     while (newNode.getParent() !== null) {
       // 구현 예정
+      if (this.getBigPriority(newNode.getData(), newNode.getParent().getData())) {
+        let tempData = newNode.getParent().getData();
+        newNode.getParent().setData(newNode.getData());
+        newNode.setData(tempData);
+        newNode = newNode.getParent();
+      } else {
+        break;
+      }
     }
   }
 
   getInsertingParentNode() {
-    // 구현 예정
+    // #1. lastInsertedNode가 rootNode인 경우
+    if (this.lastInsertedNode.getParent() === null) {
+      return this.lastInsertedNode;
+    }
+
+    // #2. lastInsertedNode가 부모 노드의 왼쪽 자식 노드인 경우
+    if (this.lastInsertedNode === this.lastInsertedNode.getParent().getLeftSubTree()) {
+      return this.lastInsertedNode.getParent();
+    }
+
+    // #3. lastInsertedNode가 부모 노드의 오른쪽 자식인 경우
+    let current = this.lastInsertedNode;
+    let firstRightSibling = null;
+
+    while (current.getParent().getParent() !== null) {
+      current = current.getParent();
+
+      firstRightSibling = this.getRightSibling(current);
+      if (firstRightSibling !== null) break;
+    }
+
+    // #3-1. 부모 노드 중에서 오른쪽 형제 노드를 가지는 노드가 존재하는 경우
+    if (firstRightSibling !== null) {
+      while (firstRightSibling.getLeftSubTree() !== null) {
+        firstRightSibling = firstRightSibling.getLeftSubTree();
+      }
+
+      return firstRightSibling;
+    }
+    // #3-2. 부모 노드 중에서 오른쪽 형제 노드를 가지는 노드가 존재하지 않는 경우
+    current = this.root;
+    while (current.getLeftSubTree() !== null) {
+      current = current.getLeftSubTree();
+    }
+    return current;
+
   }
 
-  getBigPriority() {
-    // 구현 예정
+  getBigPriority(first, second) {
+    // 현재 구현하고자 하는 Heap 자료구조는 최소 Heap으로 그에 맞춰 우선순위를 리턴
+    return first < second;
+  }
+
+  getRightSibling(node) {
+    if (node.getParent().getRightSubTree() !== node) {
+      return node.getParent().getRightSubTree();
+    }
+    return null;
+  }
+
+  getLeftSibling(node) {
+    if (node.getParent().getLeftSubTree() !== node) {
+      return node.getParent().getLeftSubTree();
+    }
+    return null;
   }
 }
+
+const heap = new Heap();
+
+heap.insert(4)
+heap.insert(2)
+heap.insert(5)
+heap.insert(7)
+heap.insert(1)
+
+heap.root.inOrderTraversal(heap.root);
+console.log(heap.root);
