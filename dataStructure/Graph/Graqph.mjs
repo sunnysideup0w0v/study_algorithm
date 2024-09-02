c84c2658909f8ba60607466e370b12d3431c0070
sunnysideup0w0v <723973972538@naver.com>
2024-09-02 09:00:03 +0900

    [자료구조] Graph의 기본 개념과 구현

---
+++ dataStructure/Graph/Graqph.mjs
@@ -0,0 +1,43 @@
+class Vertex {
+  constructor(value) {
+    this.value = value;
+    this.adjacent_vertices = [];
+  }
+
+  addAdjacentVertex(vertex) {
+    this.adjacent_vertices.push(vertex);
+  }
+
+  removeAdjacentVertex(vertex) {
+    for (let i = 0; i < this.adjacent_vertices.length; i++) {
+      if (this.adjacent_vertices[i] == vertex) {
+        this.adjacent_vertices.splice(i--, 1);
+      }
+    }
+  }
+}
+
+let jake = new Vertex('Jake');
+let ben = new Vertex('Ben');
+let joy = new Vertex('Joy');
+let ivy = new Vertex('Ivy');
+let elin = new Vertex('Elin');
+let anna = new Vertex('Anna');
+let david = new Vertex('David');
+
+jake.addAdjacentVertex(ben);
+ben.addAdjacentVertex(jake);
+joy.addAdjacentVertex(ben);
+joy.addAdjacentVertex(ivy);
+ivy.addAdjacentVertex(joy);
+ivy.addAdjacentVertex(ben);
+elin.addAdjacentVertex(ivy);
+elin.addAdjacentVertex(anna);
+anna.addAdjacentVertex(ben);
+anna.addAdjacentVertex(david);
+anna.addAdjacentVertex(elin);
+david.addAdjacentVertex(anna);
+
+console.log(anna.adjacent_vertices);
+anna.removeAdjacentVertex(david);
+console.log(anna.adjacent_vertices);
